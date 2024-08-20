0b0eb94879823c32ef229c10f9fce8c453e85279
zomars <zomars@me.com>
2024-08-20 10:43:06 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   export let data
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit 1</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
    to read the documentation
